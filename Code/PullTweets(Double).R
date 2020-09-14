12521bab6c878c579cfab0694dd85148f966bfaf
Tanay Bhardwaj <43832878+Tanay0510@users.noreply.github.com>
2020-09-14 18:19:27 -0400

    Add files via upload

--- Code/PullTweets(Double).R
+++ Code/PullTweets(Double).R
@@ -4,9 +4,9 @@ library(rtweet)
 
 # Twitter API Authentication
 
-app.name <- "#########"
-consumer.key <- "################"
-consumer.secret <- "###################"
+app.name <- "###########"
+consumer.key <- "##############"
+consumer.secret <- "##################"
 token <- create_token (app = app.name, consumer_key = consumer.key, consumer_secret = consumer.secret)
 
 # Declare Name (1)
@@ -29,9 +29,9 @@ name2 <<- "Popeyes"
 
 ## ISO Language Codes (https://www.iso.org/iso-639-language-codes.html)
 
-nk <- search_tweets(q = "chicfila OR chickfila", n = 1000, include_rts = FALSE, token = token, lang="en", type = "mixed", retryonratelimit = FALSE)
+FirstSearch <- search_tweets(q = "chicfila OR chickfila", n = 1000, include_rts = FALSE, token = token, lang="en", type = "mixed", retryonratelimit = FALSE)
 
-cbo <- search_tweets(q = "popeye OR popeyes", n = 1000, include_rts = FALSE, token = token, lang="en", type = "mixed", retryonratelimit = FALSE)
+SecondSearch <- search_tweets(q = "popeye OR popeyes", n = 1000, include_rts = FALSE, token = token, lang="en", type = "mixed", retryonratelimit = FALSE)
 
 # https://www.rdocumentation.org/packages/rtweet/versions/0.6.9/topics/search_tweets
 
@@ -42,14 +42,14 @@ Sys.info()["sysname"]
 
 if(Sys.info()["sysname"]  == "Darwin") {
   
-  save_as_csv(nk, paste0("~/Desktop/", name1 ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
+  save_as_csv(FirstSearch, paste0("~/Desktop/", name1 ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
   
-  save_as_csv(nk, paste0("~/Desktop/", name2 ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
+  save_as_csv(SecondSearch, paste0("~/Desktop/", name2 ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
   
 } else {
   
-  save_as_csv(nk, paste0("~\\Desktop\\", name1 ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
+  save_as_csv(FirstSearch, paste0("~\\Desktop\\", name1 ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
   
-  save_as_csv(nk, paste0("~\\Desktop\\", name2 ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
+  save_as_csv(SecondSearch, paste0("~\\Desktop\\", name2 ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
   
 }
