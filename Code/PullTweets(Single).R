223af9fa985297b00a94b5db96478ce9fa200111
Tanay Bhardwaj <43832878+Tanay0510@users.noreply.github.com>
2020-09-14 15:15:25 -0400

    Add files via upload

---
+++ Code/PullTweets(Single).R
@@ -0,0 +1,45 @@
+# Include Libraries
+
+library(rtweet)
+
+# Twitter API Authentication
+
+app.name <- "Text-Sentiment Analysis"
+consumer.key <- "csvtGXfIyQE1GCRhkQdy7QMN3"
+consumer.secret <- "XpDgf4yiorV4YrwFI6ga6dFtDzGhl6YSqp3sHAPCRXOGjcemJW"
+token <- create_token (app = app.name, consumer_key = consumer.key, consumer_secret = consumer.secret)
+
+# Declare Name
+
+name <<- "Chicfila"
+
+# Declare Tweet Query - Not Case Sensitive
+
+# Options
+
+# Option 1 - Include Retweets => include_rts (TRUE / FALSE)
+
+# Option 2 - Tweet Type => type (recent / mixed / popular)
+
+# Option 3 - Language => lang 
+
+## ISO Language Codes (https://www.iso.org/iso-639-language-codes.html)
+
+nk <- search_tweets(q = "chicfila OR chickfila", n = 10000, include_rts = FALSE, token = token, lang="en", type = "mixed", retryonratelimit = FALSE)
+
+# https://www.rdocumentation.org/packages/rtweet/versions/0.6.9/topics/search_tweets
+
+# Save Tweets as CSV File to Desktop
+# Check for the operating system (Windows or Mac)
+
+Sys.info()["sysname"]
+
+if(Sys.info()["sysname"]  == "Darwin") {
+  
+  save_as_csv(nk, paste0("~/Desktop/", name ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
+  
+} else {
+  
+  save_as_csv(nk, paste0("~\\Desktop\\", name ,"-Tweets ", format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
+  
+}
